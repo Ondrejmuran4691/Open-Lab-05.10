4729a4fbf49e9b573ee7caea5adc2c3c5ce9fd7f
Ondrejmuran4691 <58992354+Ondrejmuran4691@users.noreply.github.com>
2021-12-28 20:44:37 +0100

    multiples first digit of number by second and so on

---
+++ Open-Lab-05.10/Open-Lab-05.10/Challenge.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+namespace Open_Lab_05._10
+{
+    public class Challenge
+    {
+        public int MysteryFunc(int num)
+        {
+            int final = 1;
+            string one = "";
+            foreach (char c in num.ToString())
+            {
+               one = c.ToString();
+                final = final * int.Parse(one);
+            }
+            
+            return final;
+        }
+    }
+}
---
+++ Open-Lab-05.10/Open-Lab-05.10/Tests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Open_Lab_05._10
+{
+    [TestFixture]
+    public class Tests
+    {
+
+        private Challenge challenge;
+
+        private const int RandSeed = 510510510;
+        private const int RandTestCasesCount = 96;
+
+        [OneTimeSetUp]
+        public void Init() => challenge = new Challenge();
+
+        [TestCase(152, 10)]
+        [TestCase(832, 48)]
+        [TestCase(19, 9)]
+        [TestCase(133, 9)]
+        [TestCaseSource(nameof(GetRandom))]
+        public void ChallengeTest(int num, int expected) =>
+            Assert.That(challenge.MysteryFunc(num), Is.EqualTo(expected));
+
+        private static IEnumerable GetRandom()
+        {
+            var rand = new Random(RandSeed);
+
+            for (var t = 0; t < RandTestCasesCount; t++)
+            {
+                var count = rand.Next(1, 10);
+
+                var chars = new char[count];
+                var digits = new int[count];
+
+                for (var i = 0; i < count; i++)
+                {
+                    digits[i] = rand.Next(1, 10);
+                    chars[i] = (char) (digits[i] + '0');
+                }
+
+                yield return new TestCaseData(int.Parse(chars), digits.Aggregate((n1, n2) => n1 * n2));
+            }
+        }
+
+    }
+}
